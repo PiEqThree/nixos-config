eade446f7ce727244d63f6b7239fb67c4badf5f4
PiEqThree <pieqthree@proton.me>
2025-09-07 09:25:30 +0200

    wip disko

--- flake.nix
+++ flake.nix
@@ -8,13 +8,19 @@
        url = "github:nix-community/home-manager";
        inputs.nixpkgs.follows = "nixpkgs";
      };
+
+    disko = {
+       url = "github:nix-community/disko";
+       inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, disko, home-manager, nixpkgs, ... }@inputs: {
     nixosConfigurations.kelbi = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./hosts/kelbi/configuration.nix
+	inputs.disko.nixosModules.disko
 	inputs.home-manager.nixosModules.default
       ];
     };
--- hosts/kelbi/hardware-configuration.nix
+++ hosts/kelbi/hardware-configuration.nix
@@ -13,19 +13,6 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/fc11d603-edfe-4b2a-8224-380ffff55030";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1073-7F3B";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
-
-  swapDevices = [ ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
